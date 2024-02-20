51b235472d06600f5ed18739bb7a33cfddeed582
Tae Woo Kim <taewookim02@gmail.com>
2024-02-20 18:37:41 +0900

    Add home page with vanila javascript

--- src/index.js
+++ src/index.js
@@ -1,1 +1,64 @@
-console.log("Hello, world!");
+// selectors
+const body = document.querySelector("body");
+const header = document.querySelector("header");
+const nav = document.querySelector(".nav");
+const content = document.querySelector("#content");
+
+// event listeners
+document.addEventListener("DOMContentLoaded", () => {
+  addBody();
+});
+
+function addBody() {
+  const container = document.createElement("div");
+  container.classList.add("container");
+  body.appendChild(container);
+  container.appendChild(header);
+  container.appendChild(content);
+
+  // Add logo image
+  const imgLogo = document.createElement("img");
+  imgLogo.src = "../src/img/logo.png";
+  imgLogo.alt = "logo";
+  imgLogo.classList.add("img-logo");
+  header.appendChild(imgLogo);
+
+  // Add nav
+  const buttonHome = document.createElement("button");
+  buttonHome.textContent = "Home";
+  nav.appendChild(buttonHome);
+  const buttonMenu = document.createElement("button");
+  buttonMenu.textContent = "Menu";
+  nav.appendChild(buttonMenu);
+  const buttonAbout = document.createElement("button");
+  buttonAbout.textContent = "About";
+  nav.appendChild(buttonAbout);
+  header.appendChild(nav);
+
+  // Add hero
+  const hero = document.createElement("section");
+  hero.classList.add("hero");
+  content.appendChild(hero);
+  const imgHero = document.createElement("img");
+  imgHero.src = "../src/img/sushi.jpeg";
+  imgHero.alt = "sushi picture";
+  imgHero.classList.add("img-hero");
+  hero.appendChild(imgHero);
+
+  // Add introduction
+  const introduction = document.createElement("section");
+  introduction.classList.add("introduction");
+  content.appendChild(introduction);
+  const introductionTitle = document.createElement("h2");
+  introductionTitle.textContent = "Welcome to";
+  introduction.appendChild(introductionTitle);
+  const introductionParagraph1 = document.createElement("p");
+  introductionParagraph1.textContent = "Sushi for All";
+  introduction.appendChild(introductionParagraph1);
+  const introductionParagraph2 = document.createElement("p");
+  introductionParagraph2.textContent =
+    "lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum non minima earum, neque veniam fuga voluptates ipsum error mollitia velit rem maxime repellendus consequuntur, illo nobis vero dolores unde officia soluta doloremque facilis perspiciatis! Magni, optio minus. Recusandae perferendis aspernatur pariatur vero maxime atque mollitia, dolor at doloremque saepe minima.";
+  introduction.appendChild(introductionParagraph2);
+
+  console.log("body added");
+}
