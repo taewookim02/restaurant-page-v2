decc571a922bbe5202d52dd32eef89198f9e288f
Tae Woo Kim <taewookim02@gmail.com>
2024-02-20 18:57:24 +0900

    Add menu page

--- src/index.js
+++ src/index.js
@@ -6,16 +6,69 @@ const content = document.querySelector("#content");
 
 // event listeners
 document.addEventListener("DOMContentLoaded", () => {
-  addBody();
-});
-
-function addBody() {
   const container = document.createElement("div");
   container.classList.add("container");
   body.appendChild(container);
   container.appendChild(header);
   container.appendChild(content);
 
+  addHeader();
+
+  // addBody();
+  addMenu();
+});
+
+function addMenu() {
+  const menu = document.createElement("section");
+  menu.classList.add("menu");
+  content.appendChild(menu);
+
+  // add multiple menu items
+  const menuItems = [
+    {
+      name: "Sushi 1",
+      description: "lorem ipsum dolor sit amet consectetur adipisicing elit",
+      price: 10,
+      src: "../src/img/sushi1.jpg",
+    },
+    {
+      name: "Sushi 2",
+      description: "lorem ipsum dolor sit amet consectetur adipisicing elit",
+      price: 20,
+      src: "../src/img/sushi2.jpeg",
+    },
+    {
+      name: "Sushi 3",
+      description: "lorem ipsum dolor sit amet consectetur adipisicing elit",
+      price: 30,
+      src: "../src/img/sushi3.jpeg",
+    },
+  ];
+
+  menuItems.forEach((item, i) => {
+    const menuItem = document.createElement("div");
+    const menuItemImg = document.createElement("div");
+    menuItemImg.classList.add("menu-img");
+    const menuItemText = document.createElement("div");
+    menuItemText.classList.add("menu-text");
+
+    menuItem.appendChild(menuItemImg);
+    menuItem.appendChild(menuItemText);
+    menuItem.classList.add("menu-item");
+    menuItemText.innerHTML = `
+      <h3>${item.name}</h3>
+      <p>${item.description}</p>
+      <p class="menu-price">$${item.price}</p>
+    `;
+
+    menuItemImg.innerHTML = `
+      <img src="${item.src}" alt="Sushi ${i}" class="img-menu-item" />
+    `;
+    menu.appendChild(menuItem);
+  });
+}
+
+function addHeader() {
   // Add logo image
   const imgLogo = document.createElement("img");
   imgLogo.src = "../src/img/logo.png";
@@ -34,7 +87,9 @@ function addBody() {
   buttonAbout.textContent = "About";
   nav.appendChild(buttonAbout);
   header.appendChild(nav);
+}
 
+function addBody() {
   // Add hero
   const hero = document.createElement("section");
   hero.classList.add("hero");
