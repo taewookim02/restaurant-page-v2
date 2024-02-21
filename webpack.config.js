d54a9ba6ece1c2bb569aca9039614f225a546749
Tae Woo Kim <taewookim02@gmail.com>
2024-02-21 18:49:46 +0900

    Clean up webpack before production

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const path = require("path");
 
 module.exports = {
-  mode: "development",
-  devtool: "inline-source-map",
+  mode: "production",
+  // devtool: "inline-source-map",
   entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
 };
